2c3c2c537e94ec9f95c5d99d161cb13ff5181515
Bas Baccarne <Bastiaan.Baccarne@UGent.be>
2016-05-17 17:45:32 +0200

    Generic mySQL input fields

--- interface/server.R
+++ interface/server.R
@@ -10,10 +10,49 @@ shinyServer(function(input, output, session) {
                 paste("Hello ", query, ", welcome back to your personal research environment.", sep="")
         })
         
+        # Fetch the plot/data related to the ID in the url
         output$plot <- renderPlot({
                 source("dummydata.R")
                 library(ggplot2)
                 data <- getdata()
                 qplot(mpg, wt, data = data, colour = as.factor(cyl))
         })
+        
+        # Store respondent feedback
+        library(RMySQL)
+        
+        options(mysql = list(
+                "host" = "localhost",
+                "user" = "root",
+                "password" = "mysql"
+        ))
+        databaseName <- "shinydatabase"
+        table <- "responses"
+        
+        saveData <- function(data) {
+                # Connect to the database
+                db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                                port = options()$mysql$port, user = options()$mysql$user, 
+                                password = options()$mysql$password)
+                # Construct the update query by looping over the data fields
+                query <- sprintf("INSERT INTO %s (%s) VALUES ('%s')", table,input$var1,input$var2)
+                print(query)
+                # Submit the update query and disconnect
+                dbGetQuery(db, query)
+                dbDisconnect(db)
+        }
+        
+        loadData <- function() {
+                # Connect to the database
+                db <- dbConnect(MySQL(), dbname = databaseName, host =    options()$mysql$host, 
+                                port = options()$mysql$port, user = options()$mysql$user, 
+                                password = options()$mysql$password)
+                # Construct the fetching query
+                query <- sprintf("SELECT * FROM %s", table)
+                # Submit the fetch query and disconnect
+                data <- dbGetQuery(db, query)
+                dbDisconnect(db)
+                data
+                
+        }
 })
--- interface/ui.R
+++ interface/ui.R
@@ -1,8 +1,19 @@
 shinyUI(
         bootstrapPage(
         
+                # intro text
                 verbatimTextOutput("queryText"),
+                
+                # content
                 p("On this page you can find your personal research data. How would you interpret this?"),
-                plotOutput("plot")
+                plotOutput("plot"),
+                
+                # respondent input
+                fluidPage(div(
+                        textInput("var1","Variable 1",""),
+                        numericInput("var2","Variable 2",""),
+                        actionButton("submit","Submit","")
+                ))
+                
         )
 )
