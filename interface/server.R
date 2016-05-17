804b1c0e91e094472a0fbc7d1499a71459f17b5b
Bas Baccarne <Bastiaan.Baccarne@UGent.be>
2016-05-17 13:21:07 +0200

    grab data from URL

--- interface/server.R
+++ interface/server.R
@@ -1,11 +1,13 @@
 shinyServer(function(input, output, session) {
         
         # Parse the GET query string
+        # Provide data though format url?variable1=value1&variable2=value2
+        # In this case ?name=id
         output$queryText <- renderText({
                 query <- parseQueryString(session$clientData$url_search)
                 
                 # Return a string with key-value pairs
-                paste("Hello ", query, ", welcome back in your personal research environment.", sep="")
+                paste("Hello ", query, ", welcome back to your personal research environment.", sep="")
         })
         
         output$plot <- renderPlot({
